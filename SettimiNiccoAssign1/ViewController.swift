a0734f97aed3ccc83ffeb27008a2a946b493f3aa
minesettimi <minesettimi@gmail.com>
2026-02-03 19:34:58 -0500

    Initial Commit

---
+++ SettimiNiccoAssign1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettimiNiccoAssign1
+//
+//  Created by Settimi, Niccolo B. on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
